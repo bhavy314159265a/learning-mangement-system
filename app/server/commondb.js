23d447e5d693b3ae92c59208099e42e7c48a579d
thelazydude <khanna.micky01@gmail.com>
2018-05-08 16:29:57 +0530

    first commit

---
+++ app/server/commondb.js
@@ -0,0 +1,20 @@
+// 'use strict';
+// var mongoose=require("mongoose");
+// mongoose.connect("mongodb://admin:admin@ds159509.mlab.com:59509/lms");
+// var Schema=mongoose.Schema;
+// var userSchema=new Schema();
+// var User=mongoose.model("users",userSchema);
+// module.exports=User;
+
+/*
+ 'use strict';
+ var mongoose = require("mongoose");
+ // Step -2 Connect to the DB
+ mongoose.connect("mongodb://localhost:27017/LMS");
+ // Step -3 Represent Schema
+ var Schema = mongoose.Schema;
+ // Step -4  Creating Schema for the Collection
+ var userSchema= new Schema({});
+ var User = mongoose.model("users",userSchema);  // User Object Represent users Collection/!*
+ module.exports=User;
+ */
